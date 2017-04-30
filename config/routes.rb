7da8ac9524e1899b77fb7104ffac67661109969e
Keisuke Nagai <yee_7798@yahoo.co.jp>
2017-04-30 12:35:34 +0000

    fix model name

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-  root to: 'tasklists#index'
+  root to: 'tasks#index'
   
   # CRUD
-  # get 'tasklists/:id', to: 'tasklists#show'
-  # post 'tasklists', to: 'tasklists#create'
-  # put 'tasklists/:id', to: 'tasklists#update'
-  # delete 'tasklists/:id', to: 'tasklists#destroy'
+  # get 'tasks/:id', to: 'tasks#show'
+  # post 'tasks', to: 'tasks#create'
+  # put 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
   
   # index: show の補助ページ
-  # get 'tasklists', to: 'tasklists#index'
+  # get 'tasks', to: 'tasks#index'
   
   # new: 新規作成用のフォームページ
-  # get 'tasklists/new', to: 'tasklists#new'
+  # get 'tasks/new', to: 'tasks#new'
   
   # edit: 更新用のフォームページ
-  # get 'tasklists/:id/edit', to: 'tasklists#edit'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
   
   #上記はすべて下記１行で置き換えられる
-  resources :tasklists
+  resources :tasks
   
 end
