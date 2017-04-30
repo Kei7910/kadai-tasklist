4fe0d6604ea1fbe499334cd1e1aaf590bc4d3787
Keisuke Nagai <yee_7798@yahoo.co.jp>
2017-04-30 08:58:17 +0000

    kadai-tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasklists#index'
+  
+  # CRUD
+  # get 'tasklists/:id', to: 'tasklists#show'
+  # post 'tasklists', to: 'tasklists#create'
+  # put 'tasklists/:id', to: 'tasklists#update'
+  # delete 'tasklists/:id', to: 'tasklists#destroy'
+  
+  # index: show の補助ページ
+  # get 'tasklists', to: 'tasklists#index'
+  
+  # new: 新規作成用のフォームページ
+  # get 'tasklists/new', to: 'tasklists#new'
+  
+  # edit: 更新用のフォームページ
+  # get 'tasklists/:id/edit', to: 'tasklists#edit'
+  
+  #上記はすべて下記１行で置き換えられる
+  resources :tasklists
+  
 end
